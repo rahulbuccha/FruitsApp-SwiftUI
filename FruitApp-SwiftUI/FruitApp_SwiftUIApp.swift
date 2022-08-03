643739bb06f775332e40344549a9a7ad0e5d8c75
Rahul Shaw <rahulbucha69@gmail.com>
2022-08-03 12:59:21 +0530

    Initial Commit

---
+++ FruitApp-SwiftUI/FruitApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp_SwiftUIApp.swift
+//  FruitApp-SwiftUI
+//
+//  Created by Admin on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
